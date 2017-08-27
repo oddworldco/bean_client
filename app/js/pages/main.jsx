b213065de4efa8a358cb5714c9a33fba9acc2ef5
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2017-08-27 15:47:04 -0500

    updates post url

--- app/js/pages/main.jsx
+++ app/js/pages/main.jsx
@@ -53,7 +53,7 @@ export default class Main extends React.Component {
         }
         //http://localhost:3000/collect_data
         //'https://oddworld.herokuapp.com/collect_data'
-        axios.post('http://localhost:3000/collect_data', {  //CHANGE BACK
+        axios.post('https://oddworld.herokuapp.com/collect_data', {  //CHANGE BACK
             'uuid': uuid,
             'timeStamp': currentDate,
             'temp': temp,

09ea48f1a42e35a4cde14d99a998a8511336073c
joseph slater <>
2017-11-19 14:55:35 -0600

    updates

--- server.js
+++ server.js
@@ -2,16 +2,12 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
 // Serve up static files
-// app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '../dist'));
 
-// app.get('/', function (req, res) {
-// 	res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname + '../dist', 'index.html'));
+});
 
 var port = process.env.PORT || 3000;
 

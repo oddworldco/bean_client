4be62967f990282700245db8bd91e6428955d50c
joseph slater <>
2017-11-19 14:29:19 -0600

    updates

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
+// var express = require('express');
+// var path = require('path');
+// var app = express();
+// const PORT = process.env.PORT || 4205
+
+// // Serve up static files
+// app.use(express.static(__dirname + '/dist'));
+
+// app.get('/', function (req, res) {
+// 	res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
+
+// // Start Server
+// app.listen(PORT);
+
 var express = require('express');
 var path = require('path');
 var app = express();
-const PORT = process.env.PORT || 4205
 
-// Serve up static files
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-// Start Server
-app.listen(PORT);
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});

c1a3dd7c351fac5da198b647ff4040c64e1a95b7
joseph slater <>
2017-11-19 00:29:41 -0600

    test

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var path = require('path');
 var app = express();
 
 // Serve up static files
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + 'dist'));
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname + '/dist/index.html'));
+	res.sendFile(path.join(__dirname + 'dist/index.html'));
 });
 
 // Start Server

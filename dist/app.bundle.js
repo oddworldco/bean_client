b52c5334f6e71ad15e3496ffaba4c41f5ddc9a38
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2017-11-19 23:06:04 -0600

    adds bean

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -981,7 +981,7 @@ var Hello = function (_Component) {
       return _react2.default.createElement(
         'div',
         null,
-        'Hello from react!!! push 2',
+        'Hello from react!!! push 3',
         _react2.default.createElement('img', { src: _keen2.default, alt: 'Commander Keen' })
       );
     }
